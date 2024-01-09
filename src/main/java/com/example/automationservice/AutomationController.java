6d25d8c71b9e4e6533fc147133168ecbd9d9993c
G00405094 <G00405094@atu.ie>
2024-01-09 21:11:51 +0000

    Created Contoller

---
+++ src/main/java/com/example/automationservice/AutomationController.java
@@ -0,0 +1,26 @@
+package com.example.automationservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class AutomationController {
+    private final NotificationService notificationService;
+
+    @Autowired
+    public RegistrationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping("/confirm")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Map<String, String> handleRegistrationRequests(@RequestBody DeviceDetails deviceDetails) {
+        System.out.println("User Details received at Registration Controller: " + deviceDetails);
+        return notificationService.noti(deviceDetails);
+}
